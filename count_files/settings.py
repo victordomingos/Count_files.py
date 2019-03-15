3bbb205b22f0bda101f61183e071ed73fece304e
NataliaBondarenko <write_up@mail.ua>
2019-03-15 18:26:17 +0200

    added SUPPORTED_PATTERN_MESSAGE(find group), WARNING if incompatible arguments are entered(basic checks for main groups), updated ST messages(appearance)

--- count_files/settings.py
+++ count_files/settings.py
@@ -64,14 +64,58 @@ def simple_columns(text_input, num_columns=4):
     return text_table
 
 
-SUPPORTED_TYPE_INFO_MESSAGE = f'\nThis is the list of currently supported file types for preview:\n\n' \
+SUPPORTED_TYPE_INFO_MESSAGE = '\nThis is the list of currently supported file types for preview:\n\n' \
                               f'{simple_columns(SUPPORTED_TYPES["text"], num_columns=4)}\n' \
-                              f'Previewing files without extension is not supported. ' \
-                              f'You can use the "--preview" argument together with the search ' \
-                              f'for all files regardless of the extension ("--file-extension .."). ' \
-                              f'In this case, the preview will only be displayed for files with a supported extension.\n\n'
+                              'Previewing files without extension is not supported.\n' \
+                              'You can use the "--preview" argument together with the search ' \
+                              'for all files regardless of the extension ("--file-extension ..").\n' \
+                              'In this case, the preview will only be displayed for files ' \
+                              'with a supported extension.\n\n'
 
-NOT_SUPPORTED_TYPE_MESSAGE = f'\nSorry, there is no preview available for this file type. ' \
-                             f'You may want to try again without preview. ' \
-                             f'This is the list of currently supported file types for preview:\n\n' \
+NOT_SUPPORTED_TYPE_MESSAGE = '\nSorry, there is no preview available for this file type.\n' \
+                             'You may want to try again without preview.\n' \
+                             'This is the list of currently supported file types for preview:\n\n' \
                              f'{simple_columns(SUPPORTED_TYPES["text"], num_columns=4)}\n'
+
+
+SUPPORTED_PATTERN_MESSAGE = '\nExamples of usage with substitute character "*" ' \
+                            'that means "any number of any characters".\n' \
+                            'Can be used with: ' \
+                            '--path-contains, --filename-contains, --extension-contains\n' \
+                            'Help: count-files --args-help find\n' \
+                            'Extension starts with substring:\n' \
+                            '    count-files --extension-contains substring*\n' \
+                            'Filename ends with substring:\n' \
+                            '    count-files --filename-contains *substring\n' \
+                            'Path contains substring:\n' \
+                            '    count-files --path-contains *substring*\n' \
+                            '    or simply\n' \
+                            '    count-files --path-contains substring\n' \
+                            'Search for "*" itself:\n' \
+                            '    count-files --path-contains * (starts with "*")\n' \
+                            '    count-files --path-contains ** (ends with "*")\n' \
+                            '    count-files --path-contains *** (contains one "*")\n' \
+                            'Search is case insensitive by default. ' \
+                            'You can use the --case-sensitive argument.\n' \
+                            'The substring to search for may contain ' \
+                            'some special characters inside (sub.str*ing).\n' \
+                            'In this case character simply means the character itself.\n' \
+                            'Some characters may have special meaning for the terminal. ' \
+                            'If the substring to search for contains them or spaces, ' \
+                            'you need to specify it in quotes.'
+
+WARNING = '\nPlease use only one of the following commands:\n' \
+          'Total number of files.\n' \
+          '    count-files --total EXTENSION <arguments>\n' \
+          '    Help: count-files --args-help total\n' \
+          'File counting by extension.\n' \
+          '    count-files <arguments>\n' \
+          '    Help: count-files --args-help count\n' \
+          'File searching by extension.\n' \
+          '    count-files --file-extension FILE_EXTENSION <arguments>\n' \
+          '    Help: count-files --args-help search\n' \
+          'Find substring in a path, file name or extension.\n' \
+          '    count-files --path-contains PATH_SUBSTRING <arguments>\n' \
+          '    count-files --filename-contains FILENAME_SUBSTRING <arguments>\n' \
+          '    count-files --extension-contains EXTENSION_SUBSTRING <arguments>\n' \
+          '    Help: count-files --args-help find\n' \
